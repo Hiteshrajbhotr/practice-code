195f85edcd4ed3197ac117b9c4f386df1443598b
hiteshbhotra <hiteshraj584@gmail.com>
2021-12-16 12:51:44 +0530

    Added new Class NewTest.

--- src/com/company/controller/NewTest.java
+++ src/com/company/controller/NewTest.java
@@ -2,6 +2,6 @@ package com.company.controller;
 
 public class NewTest {
     public static void main(String[] args) {
-        System.out.println("Second Test");
+        System.out.println("New Test Class.");
     }
 }
